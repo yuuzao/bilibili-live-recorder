b259bd88c0587bd802363f9f04d744a6d49b0965
yuuzao <sisylocke@gmail.com>
2020-03-06 01:04:46 +0800

    refactory user method

--- live/get_url.py
+++ live/get_url.py
@@ -1,68 +1,99 @@
 import requests
 import re
 import os
+from yarl import URL
+
+from live.param import Api, Header
 from live.log import log
 
 
-def get_room_info(user_id):
-    url = f"https://api.live.bilibili.com/room/v1/Room/getRoomInfoOld?mid={user_id}"
-    user_info = requests.get(url)
+class User:
+    def __init__(self, id_info, qn=None):
+        self.user_name = None
+        self.room_title = None
+        self.qn = qn
 
-    code = user_info.status_code
-    if not code == 200:
-        log.error(f"request user info failed, status {code}")
-        return None
+        self.live_status = 0
+        self.stream_url = None
 
-    if not user_info.json()["data"]["roomStatus"]:
-        log.error("space info error, skip")
-        return None
+        self.api = Api()
+        if 'user_id' in id_info:
+            self.flag = True
 
-    return user_info.json()
+            self.uid = id_info['user_info']
+            self.api.set_uid(self.uid)
+            self.get_user_name()
+            self.get_room_from_uid()
+
+        if 'room_id' in id_info and self.flag is not True:
+            self.mid = id_info['room_id']
+            self.api.set_mid(self.mid)
+
+    def get_user_name(self):
+        res = self.req(self.api.user_info)
+        if not res['err']:
+            user = res['res']
+            self.user_name = user.json()['data']['name']
+            log.info(f'get user name: {self.user_name}')
+        else:
+            log.info(f"get user name failed, status {res['code']}")
+
+    def get_room_from_uid(self):
+        res = self.req(self.api.room_from_uid)
+        if not res['err']:
+            room = res['res']
+            room_url = URL(room['data']['url'])
+
+            self.mid = room_url.parts[-1]
+            self.room_title = room['data']['title']
+            self.live_status = room['data']['liveStatus']
+            log.info(f'get room title: {self.room_title}, live status {self.live_status}')
+        
+        else:
+            log.info(f"get room info failed, status {res['code']}")
 
 
-def get_room_id(room_info):
-    room_url = room_info["data"]["url"]
-    room_id = re.findall(r"\d+", room_url)[0]
-    log.info(f"room_id is {room_id}")
+    def get_room_from_mid(self):
+        res = self.req(self.api.room_info)
+        if not res['err']:
+            room = res['res']
+            self.uid = room['data']['uid']
+            self.room_title = room['data']['title']
+            self.live_status = room['data']['live_status']
+            log.info(f'get room title: {self.room_title}, live status {self.live_status}')
+            self.get_user_name()
 
-    return room_id
+        else:
+            log.info(f"get room info failed, status {res['code']}")
+
+    def get_live_status(self):
+        res = self.req(self.api.live_staus)
+
+        if not res['err']:
+            # staus == 1 -> live on; staus == 0 -> live off
+            self.live_status = res['res']['data']['live_status']
+            log.info(f"room {self.room_title} in status {self.live_status}")
+        
+        else:
+            log.info(f'get live status failed, status {res["code"]}')
+    
+    def get_stream_url(self):
+        res = self.req(self.api.stream_url)
+        if not res['err']:
+            self.stream_url = res['res']['data']['play_url']['durl'][0]
+            log.info(f"get stream url {self.stream_url}")
+        
+        else:
+            log.info(f"fetch live stream failed, status {res['code']}")
 
 
-def get_room_title(room_info):
-    title = room_info["data"]["title"]
-    log.info(f"room title is {title}")
-    return title
+    @staticmethod
+    def req(url):
+        headers = Header(url)
+        res = requests.get(url, headers=headers)
 
+        code = res.status_code
+        if code < 400:
+            return {'err': True, "code": code}
 
-def get_live_status(room_id):
-    url = f"https://api.live.bilibili.com/room/v1/Room/room_init?id={room_id}"
-    res = requests.get(url)
-
-    code = res.status_code
-    if not code == 200:
-        log.error(f"request live status failed, status {code}")
-        return None
-
-    # status == 1 -> live on; status ==0 -> live off
-    live_status = res.json()["data"]["live_status"]
-    return live_status
-
-
-def get_stream_url(room_id):
-    quality = 10000  # 原画画质
-    stream_info_url = (
-        "https://api.live.bilibili.com/xlive/web-room/v1/index/"
-        f"getRoomPlayInfo?room_id={room_id}&play_url=1&mask=1&"
-        f"qn={quality}&platform=web"
-    )
-    res = requests.get(stream_info_url)
-
-    code = res.status_code
-    if not code == 200:
-        log.error(f"parse stream url failed, status {code}")
-        return None
-
-    stream_info = res.json()
-    stream_url = stream_info["data"]["play_url"]["durl"][0]["url"]
-
-    return stream_url
+        return {'err': False, 'res': res.json()}
