e98542d9c7a45e1833f6e023fa5598a8636e2181
yuuzao <sisylocke@gmail.com>
2020-03-13 22:21:31 +0800

    set readme and url in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name="bilibili-live-recorder",
-    version="0.4",
+    version="0.7.1",
     packages=["live"],
     platforms=["unix"],
     install_requires=["requests", "loguru", "yarl"],
     entry_points={"console_scripts": ["blr = live.main:main",]},
     author="yuzao",
     description="This is a script to record bilibili lives, coding with asyncio",
+    url="https://github.com/yuuzao/bilibili-live-recorder",
     keywords="bilibili live",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license="MIT",
 )
